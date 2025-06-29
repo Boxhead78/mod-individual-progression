1bb142b58d48158637da54f1ccff49b1277d7ee5
Boxhead78 <maik_baumann90@web.de>
2025-06-29 18:26:31 +0200

    Add IP_LEVEL_CATA

--- src/IndividualProgression.h
+++ src/IndividualProgression.h
@@ -25,7 +25,8 @@ enum ProgressionLevelThreshold
 {
     IP_LEVEL_VANILLA = 60,
     IP_LEVEL_TBC     = 70,
-    IP_LEVEL_WOTLK   = 80
+    IP_LEVEL_WOTLK   = 80,
+    IP_LEVEL_CATA    = 90
 };
 
 enum ProgressionBossIDs
